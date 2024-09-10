8c20edb785d5d821622ce8bb0dafc77124da25d6
Ian-Mark <ianarapkirui@gmail.com>
2024-09-11 01:11:25 +0300

    updates--1--ui changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,42 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/Home";
-import Hotel from "./pages/hotel/Hotel";
-import List from "./pages/list/List";
+import React from "react";
 
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import NoPage from "./pages/NoPage";
+import PlacesRoute from "./pages/PlacesRoute";
+import About from "./pages/About";
+import BlogsDetails from "./pages/BlogsDetails";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 900,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/hotels" element={<List />} />
-        <Route path="/hotels/:id" element={<Hotel />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="blogs" element={<Blogs />} />
+            <Route path="blogs/:id" element={<BlogsDetails />} />
+            <Route path="best-places" element={<PlacesRoute />} />
+            <Route path="about" element={<About />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,51 +1,153 @@
-import "./footer.css";
+import React from "react";
+import FooterLogo from "../../assets/logo.png";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaLocationArrow,
+  FaMobileAlt,
+} from "react-icons/fa";
+import NatureVid from "../../assets/video/footer.mp4";
+import { Link } from "react-router-dom";
+
+const FooterLinks = [
+  {
+    title: "Home",
+    link: "/",
+  },
+  {
+    title: "About",
+    link: "/about",
+  },
+  {
+    title: "Best Places",
+    link: "/best-places",
+  },
+  {
+    title: "Blogs",
+    link: "/blogs",
+  },
+];
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="fLists">
-        <ul className="fList">
-          <li className="fListItem">Countries</li>
-          <li className="fListItem">Regions</li>
-          <li className="fListItem">Cities</li>
-          <li className="fListItem">Districts</li>
-          <li className="fListItem">Airports</li>
-          <li className="fListItem">Hotels</li>
-        </ul>
-        <ul className="fList">
-          <li className="fListItem">Homes </li>
-          <li className="fListItem">Apartments </li>
-          <li className="fListItem">Resorts </li>
-          <li className="fListItem">Villas</li>
-          <li className="fListItem">Hostels</li>
-          <li className="fListItem">Guest houses</li>
-        </ul>
-        <ul className="fList">
-          <li className="fListItem">Unique places to stay </li>
-          <li className="fListItem">Reviews</li>
-          <li className="fListItem">Unpacked: Travel articles </li>
-          <li className="fListItem">Travel communities </li>
-          <li className="fListItem">Seasonal and holiday deals </li>
-        </ul>
-        <ul className="fList">
-          <li className="fListItem">Car rental </li>
-          <li className="fListItem">Flight Finder</li>
-          <li className="fListItem">Restaurant reservations </li>
-          <li className="fListItem">Travel Agents </li>
-        </ul>
-        <ul className="fList">
-          <li className="fListItem">Curtomer Service</li>
-          <li className="fListItem">Partner Help</li>
-          <li className="fListItem">Careers</li>
-          <li className="fListItem">Sustainability</li>
-          <li className="fListItem">Press center</li>
-          <li className="fListItem">Safety Resource Center</li>
-          <li className="fListItem">Investor relations</li>
-          <li className="fListItem">Terms & conditions</li>
-        </ul>
+    <>
+      <div className=" dark:bg-gray-950 py-10 relative overflow-hidden">
+        <video
+          autoPlay
+          loop
+          muted
+          className="absolute right-0 top-0 h-full overflow-hidden w-full object-cover z-[-1]"
+        >
+          <source src={NatureVid} type="video/mp4" />
+        </video>
+        <div className="container">
+          <div className="grid md:grid-cols-3 py-5 bg-white/80 backdrop-blur-sm rounded-t-xl">
+            <div className="py-8 px-4">
+              <h1 className="flex items-center gap-3 text-xl sm:text-3xl font-bold text-justify sm:text-left">
+                <img src={FooterLogo} alt="" className="max-h-[60px]" />
+                {/* TravelloGo */}
+              </h1>
+              <p className="text-sm">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+                facere ab hic accusamus omnis dolor voluptatibus illo, tempore
+                eum tenetur.
+              </p>
+              <br />
+              <div className="flex items-center gap-3 ">
+                <FaLocationArrow />
+                <p>Noida, Uttar Pradesh</p>
+              </div>
+              <div className="flex items-center gap-3 mt-3">
+                <FaMobileAlt />
+                <p>+91 123456789</p>
+              </div>
+              {/* social handles */}
+              <div>
+                <div className="flex items-center gap-3 mt-6">
+                  <a href="#">
+                    <FaInstagram className="text-3xl" />
+                  </a>
+                  <a href="#">
+                    <FaFacebook className="text-3xl" />
+                  </a>
+                  <a href="#">
+                    <FaLinkedin className="text-3xl" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10">
+              <div>
+                <div className="py-8 px-4">
+                  <h1 className="text-xl font-bold text-justify sm:text-left mb-3">
+                    Important Links
+                  </h1>
+                  <ul className="flex flex-col gap-3">
+                    {FooterLinks.map((link) => (
+                      <li className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-700 dark:text-gray-200">
+                        <Link
+                          to={link.link}
+                          onClick={() => window.scrollTo(0, 0)}
+                        >
+                          <span>&#11162;</span>
+                          <span>{link.title}</span>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+              <div>
+                <div className="py-8 px-4">
+                  <h1 className="text-xl font-bold text-justify sm:text-left mb-3">
+                    Important Links
+                  </h1>
+                  <ul className="flex flex-col gap-3">
+                    {FooterLinks.map((link) => (
+                      <li className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-700 dark:text-gray-200">
+                        <Link
+                          to={link.link}
+                          onClick={() => window.scrollTo(0, 0)}
+                        >
+                          <span>&#11162;</span>
+                          <span>{link.title}</span>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+              <div>
+                <div className="py-8 px-4">
+                  <h1 className="text-xl font-bold text-justify sm:text-left mb-3">
+                    Important Links
+                  </h1>
+                  <ul className="flex flex-col gap-3">
+                    {FooterLinks.map((link) => (
+                      <li className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-700 dark:text-gray-200">
+                        <Link
+                          to={link.link}
+                          onClick={() => window.scrollTo(0, 0)}
+                        >
+                          <span>&#11162;</span>
+                          <span>{link.title}</span>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="text-center py-5 border-t-2 border-gray-300/50 bg-primary text-white">
+              @copyright 2024 All rights reserved || Made with ❤️ by Dilshad
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="fText">Copyright © 2022 Lamabooking.</div>
-    </div>
+    </>
   );
 };
 
